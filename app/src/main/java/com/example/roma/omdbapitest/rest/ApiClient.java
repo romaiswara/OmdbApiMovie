98f5e26629115b09daf059a8add43997f268d9c6
roma <kanikolo55>
2019-01-30 20:16:15 +0700

    add log retrofit

--- app/src/main/java/com/example/roma/omdbapitest/rest/ApiClient.java
+++ app/src/main/java/com/example/roma/omdbapitest/rest/ApiClient.java
@@ -1,19 +1,37 @@
 package com.example.roma.omdbapitest.rest;
 
+import com.example.roma.omdbapitest.BuildConfig;
+import com.ihsanbal.logging.Level;
+import com.ihsanbal.logging.LoggingInterceptor;
+
+import okhttp3.OkHttpClient;
+import okhttp3.internal.platform.Platform;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     public final static String BASE_URL = "http://www.omdbapi.com/";
-    public final static String API_KEY = "1d38a50";
 
-    private static Retrofit retrofit=null;
+    private static Retrofit retrofit = null;
 
     public static Retrofit getRetrofit() {
-        if (retrofit == null){
+        if (retrofit == null) {
+            OkHttpClient.Builder client = new OkHttpClient.Builder();
+            client.addInterceptor(new LoggingInterceptor.Builder()
+                    .loggable(BuildConfig.DEBUG)
+                    .setLevel(Level.BASIC)
+                    .log(Platform.INFO)
+                    .request("Request")
+                    .response("Response")
+                    .addHeader("version", BuildConfig.VERSION_NAME)
+                    .build());
+            OkHttpClient okHttpClient = client.build();
+
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
